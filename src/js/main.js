2def82289c5355f84a657659a29933bf3b8a56f1
Anatolii Velichko <velichko.vega@gmail.com>
2017-12-11 04:17:45 +0200

    commit

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+//= ../../node_modules/slick-carousel/slick/slick.js
+
+//= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
+
+
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
+
+$(".circle__item").click(function(){
+    $(".circle__item").addClass("circle__color");
+})
